816978b6fb520de240aa7d908bedf48debe7734e
Leo Omi <leoyuuki@gmail.com>
2023-11-28 08:51:15 -0300

    Add more sprites and add levels

--- scripts/debug/debug_info/ScaleDebug.gd
+++ scripts/debug/debug_info/ScaleDebug.gd
@@ -4,6 +4,10 @@ extends Control
 @export var scale_object: Scale
 @onready var label = $Label
 
+func _ready():
+	if not OS.is_debug_build():
+		queue_free()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	label.text = "%s" % scale_object.current_difference
--- scripts/debug/debug_info/WeightDebugUI.gd
+++ scripts/debug/debug_info/WeightDebugUI.gd
@@ -4,6 +4,10 @@ extends Control
 @export var weight: Weight
 @onready var label = $Label
 
+func _ready():
+	if not OS.is_debug_build():
+		queue_free()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	label.text = "%s" % weight.get_total_weight()
